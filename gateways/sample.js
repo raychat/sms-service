d80d6a17e9d83697d9934ff92d73bae6c3e301d3
ali5991jalali <39838728+ali5991jalali@users.noreply.github.com>
2018-11-28 15:04:53 +0330

    create sample.js
    create sample.js file and add empty functions for developers  to add new gateWay to Raychat sms service

---
+++ gateways/sample.js
@@ -0,0 +1,34 @@
+/**
+    new sms gateway
+    Author : developer name
+**/
+
+// for make http or https requests
+const request = require('request')
+
+/** 
+  @param {string} auth
+  @param {string} message
+  @param {string} sender
+  @param {string} receptor
+  @param {function} callback
+**/
+
+module.exports.sendSms = (auth, message, sender, receptor, callback) => {
+    
+    /**
+     * write send sms codes for your new sms gateway 
+     */
+
+    },
+
+    /** 
+      @param {string} apikey
+      @param {function} callback
+    **/
+
+    module.exports.getInfo = (apikey, callback) => {
+        /**
+         * write get info codes for your new sms gateway
+         */
+    }
